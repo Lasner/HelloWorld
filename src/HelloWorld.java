fe8d7eb8d369094c8c275f395e2b13628eb464eb
Lasner <Lauri.Lasner@tptlive.ee>
2016-11-16 13:32:35 +0200

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,6 +3,7 @@
  */
 public class HelloWorld {
     public static void main(String[]args){
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World!"); //println tähendab rea vahet
+        System.out.println("How are you?");
     }
 }
